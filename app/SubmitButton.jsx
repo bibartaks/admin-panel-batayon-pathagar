283f8bfb1839b709b1524113dd712c8c29df6a6a
bibartaks <bibartaks@gmail.com>
2024-02-14 00:52:25 +0600

    build: Add physics 1 and 2 paper admin panel and installed mongodb

---
+++ app/SubmitButton.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+export default function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      className="flex items-start bg-black text-white  justify-start  self-start px-2 py-1 rounded-md text-sm font-semibold opacity-80 hover:opacity-90"
+    >
+      Submit
+    </button>
+  );
+}
