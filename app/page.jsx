49043969232d29b741ae97c6a08607e4a59eddef
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2024-02-29 12:34:48 +0600

    feat: Add humanities section and added deography 1 paper content

--- app/page.jsx
+++ app/page.jsx
@@ -1,41 +1,44 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
-      <Button>Button</Button>
-      <div className="container py-10">
-        <h1 className="text-2xl font-medium text-center mb-5">
+      <div className='container py-10'>
+        <h1 className='text-2xl font-medium text-center mb-5'>
           Welcome to Batayon Pathagar Admin Panel
         </h1>
-        <h3 className="mb-5">Choose a subject:</h3>
-        <Link href="/physics_1" className="block mb-3 text-sm underline">
+        <h1 className='font-bold text-2xl mb-4'>Science</h1>
+        <h3 className='mb-3 font-semibold'>Choose a subject:</h3>
+        <Link href='/physics_1' className='block mb-3 text-sm underline'>
           Physics First Paper
         </Link>
-        <Link href="/physics_2" className="block mb-3 text-sm underline">
+        <Link href='/physics_2' className='block mb-3 text-sm underline'>
           Physics Second Paper
         </Link>
-        <Link href="/higher_math_1" className="block mb-3 text-sm underline">
+        <Link href='/higher_math_1' className='block mb-3 text-sm underline'>
           Higher Math First Paper
         </Link>
-        <Link href="/higher_math_2" className="block mb-3 text-sm underline">
+        <Link href='/higher_math_2' className='block mb-3 text-sm underline'>
           Higher Math Second Paper
         </Link>
-        <Link href="/chemistry_1" className="block mb-3 text-sm underline">
+        <Link href='/chemistry_1' className='block mb-3 text-sm underline'>
           Chemistry First Paper
         </Link>
-        <Link href="/chemistry_2" className="block mb-3 text-sm underline">
+        <Link href='/chemistry_2' className='block mb-3 text-sm underline'>
           Chemistry Second Paper
         </Link>
-        <Link href="/biology_1" className="block mb-3 text-sm underline">
+        <Link href='/biology_1' className='block mb-3 text-sm underline'>
           Biology 1
         </Link>
-        <Link href="/biology_2" className="block mb-3 text-sm underline">
+        <Link href='/biology_2' className='block mb-3 text-sm underline'>
           Biology 2
         </Link>
-        <Link href="/ict" className="block mb-3 text-sm underline">
+        <Link href='/ict' className='block mb-3 text-sm underline'>
           ICT
+        </Link>
+        <h1 className='font-bold text-2xl mb-4'>Humanities</h1>
+        <Link href='/geography_1' className='block mb-3 text-sm underline'>
+          Geography 1
         </Link>
       </div>
     </main>
